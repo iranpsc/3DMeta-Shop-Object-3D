57eb611301a9564719d3bc944002527942b806fe
abbass <abbas.ajorlou1371@gmail.com>
2026-07-15 22:05:28 +0330

    Fix sitemap URL generation in Category model by updating route structure to use the correct format.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -61,7 +61,7 @@ class Category extends Model implements Sitemapable
      */
     public function toSitemapTag(): Url|string|array
     {
-        $url = Url::create(url('categories.show/' . $this->url))
+        $url = Url::create(url('categories/' . $this->url))
             ->setLastModificationDate($this->updated_at)
             ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
             ->setPriority(0.8);
