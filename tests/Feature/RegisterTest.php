8dcfbbddc1a6fa25840a2900f9417b8937f0e1a1
abbass <abbas.ajorlou1371@gmail.com>
2026-08-01 17:58:12 +0330

    Remove obsolete test files for Category, Order, Product, and related functionalities
    - Deleted CategoryTest, OrderTest, ProductTest, and related methods from ProductImportTest to streamline the test suite.
    - This cleanup helps maintain focus on relevant tests and improves overall project organization.

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -8,16 +8,15 @@ class RegisterTest extends TestCase
 {
     public function test_register_redirects_to_oauth_server()
     {
-        // .env
         config([
-            'app.oauth_client_id' => env('OAUTH_CLIENT_ID', '19'),
-            'app.oauth_server_url' => env('OAUTH_SERVER_URL', 'https://accounts.irpsc.com'),
+            'app.oauth_client_id' => 'test-client-id',
+            'app.oauth_server_url' => 'https://accounts.example.com',
         ]);
-
 
         $response = $this->get(route('register'));
 
-        $expectedUrl = 'https://accounts.irpsc.com/register?client_id=19&redirect_uri=' . urlencode(route('login'));
+        $expectedUrl = 'https://accounts.example.com/register?client_id=test-client-id&redirect_uri='
+            .urlencode(route('login'));
 
         $response->assertRedirect($expectedUrl);
     }
